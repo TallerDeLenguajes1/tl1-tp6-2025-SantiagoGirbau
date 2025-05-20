d8e3ae170dd5fa74d530347ba5d3778479a5ca86
SantiagoGirbau <santiagogirbau@gmail.com>
2025-05-20 13:21:40 -0300

    Suma y leer numero

---
+++ tp6.2.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+float a;
+float b;
+
+
+string input;
+do
+{
+    Console.WriteLine("Escriba la operación a realizar s=Suma e=Exit \n");
+    input = Console.ReadLine();
+    switch (input)
+    {
+        case "s":
+        a=LeerNumero();
+        b=LeerNumero();
+        Console.WriteLine("Resultado de la Suma= "+ suma(a,b));
+            break;
+        case "e":
+            break;
+        default:
+         Console.WriteLine("No es una opción válida");
+            break;
+    }
+} while (input!="e");
+
+
+
+float LeerNumero(){
+    float a;
+    bool Resultado;
+        do{
+            Console.WriteLine("Escriba un numero: ");
+            Resultado=float.TryParse(Console.ReadLine(), out a);
+                if(!Resultado){
+                    Console.WriteLine("No es un número válido");
+                }
+        }while(!Resultado);
+    return a;
+}
+
+float suma(float a, float b){
+    return a+b;
+}
