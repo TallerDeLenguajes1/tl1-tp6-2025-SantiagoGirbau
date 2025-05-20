7070fccdc217aa10e232ac73aa6586dec3d24db0
SantiagoGirbau <santiagogirbau@gmail.com>
2025-05-20 13:32:24 -0300

    Ejercicio 2

--- tp6.2.cs
+++ tp6.2.cs
@@ -6,22 +6,41 @@ float b;
 string input;
 do
 {
-    Console.WriteLine("Escriba la operación a realizar s=Suma e=Exit \n");
+    Console.WriteLine("Escriba la operación a realizar suma, resta, mult, div o exit para salir \n");
     input = Console.ReadLine();
     switch (input)
     {
-        case "s":
-        a=LeerNumero();
-        b=LeerNumero();
-        Console.WriteLine("Resultado de la Suma= "+ suma(a,b));
+        case "suma":
+            a=LeerNumero();
+            b=LeerNumero();
+            Console.WriteLine("Resultado = "+ suma(a,b));
             break;
-        case "e":
+        case "resta":
+            a=LeerNumero();
+            b=LeerNumero();
+            Console.WriteLine("Resultado = "+ resta(a,b));
+            break;
+        case "mult":
+            a=LeerNumero();
+            b=LeerNumero();
+            Console.WriteLine("Resultado = "+ multiplicacion(a,b));
+            break;
+        case "div":
+            a=LeerNumero();
+            b=LeerNumero();
+            if(b==0){
+            Console.WriteLine("NO EXISTE LA DIVISIÓN EN CERO");
+            }else{
+            Console.WriteLine("Resultado = "+ division(a,b));
+            }
+            break;
+        case "exit":
             break;
         default:
          Console.WriteLine("No es una opción válida");
             break;
     }
-} while (input!="e");
+} while (input!="exit");
 
 
 
@@ -40,4 +59,17 @@ float LeerNumero(){
 
 float suma(float a, float b){
     return a+b;
+}
+
+float resta(float a, float b){
+    return a-b;
+}
+
+float multiplicacion(float a, float b){
+    return a*b;
+}
+
+float division(float a, float b){
+    
+    return a/b;
 }
