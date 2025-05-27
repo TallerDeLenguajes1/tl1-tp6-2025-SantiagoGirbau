76fb69639cde9c74864a1ef2502838aed4851fae
SantiagoGirbau <santiagogirbau@gmail.com>
2025-05-26 23:39:55 -0300

    merge Terminado

--- tp6.3.cs
+++ tp6.3.cs
@@ -3,36 +3,84 @@ float a;
 float b;
 
 
-Console.WriteLine("Escriba un numero para calcular el ABS, CUADRADO, RAÍZ, SENO COSENO y PARTE REAL:\n");
-
-a = LeerNumero();
-
-Console.WriteLine("Valor Absoluto = " + abs(a));
-
-Console.WriteLine("Cuadrado = " + cuadrado(a));
-
-Console.WriteLine("Raríz cuadrada = " + raizCuadrada(a));
-
-Console.WriteLine("Seno = " + seno(a));
-
-Console.WriteLine("Coseno = " + coseno(a));
-
-Console.WriteLine("Parte real = " + parteEntera(a));
-
-Console.WriteLine("Escriba dos numeros para calcular el mayor y el menor:\n");
-
-a = LeerNumero();
-b = LeerNumero();
-
-if (a != b)
+string input;
+do
 {
-    Console.WriteLine("El mayor es: " + Mayor(a, b));
-    Console.WriteLine("El menor es: " + Menor(a, b));
-}
-else
-{
-    Console.WriteLine("Los números son iguales\n");
-}
+    Console.WriteLine("Escriba la operación a realizar suma, resta, mult, div, abs, cuad, raiz, V2 para funciones extra o exit para salir \n");
+    input = Console.ReadLine();
+    switch (input)
+    {
+        case "suma":
+            a = LeerNumero();
+            b = LeerNumero();
+            Console.WriteLine("Resultado = " + suma(a, b));
+            break;
+        case "resta":
+            a = LeerNumero();
+            b = LeerNumero();
+            Console.WriteLine("Resultado = " + resta(a, b));
+            break;
+        case "mult":
+            a = LeerNumero();
+            b = LeerNumero();
+            Console.WriteLine("Resultado = " + multiplicacion(a, b));
+            break;
+        case "div":
+            a = LeerNumero();
+            b = LeerNumero();
+            if (b == 0)
+            {
+                Console.WriteLine("NO EXISTE LA DIVISIÓN EN CERO");
+            }
+            else
+            {
+                Console.WriteLine("Resultado = " + division(a, b));
+            }
+            break;
+        case "V2":
+            a = LeerNumero();
+            Console.WriteLine("Resultado = " + raizCuadrada(a));
+            break;
+            Console.WriteLine("Escriba un numero para calcular el ABS, CUADRADO, RAÍZ, SENO COSENO y PARTE REAL:\n");
+
+            a = LeerNumero();
+
+            Console.WriteLine("Valor Absoluto = " + abs(a));
+
+            Console.WriteLine("Cuadrado = " + cuadrado(a));
+
+            Console.WriteLine("Raríz cuadrada = " + raizCuadrada(a));
+
+            Console.WriteLine("Seno = " + seno(a));
+
+            Console.WriteLine("Coseno = " + coseno(a));
+
+            Console.WriteLine("Parte real = " + parteEntera(a));
+
+            Console.WriteLine("Escriba dos numeros para calcular el mayor y el menor:\n");
+
+            a = LeerNumero();
+            b = LeerNumero();
+
+            if (a != b)
+            {
+                Console.WriteLine("El mayor es: " + Mayor(a, b));
+                Console.WriteLine("El menor es: " + Menor(a, b));
+            }
+            else
+            {
+                Console.WriteLine("Los números son iguales\n");
+            }
+            break;
+        case "exit":
+            break;
+        default:
+            Console.WriteLine("No es una opción válida");
+            break;
+    }
+} while (input != "exit");
+
+
 
 float LeerNumero()
 {
@@ -50,19 +98,26 @@ float LeerNumero()
     return a;
 }
 
-float abs(float a)
+float suma(float a, float b)
 {
-    if (a >= 0)
-    {
-        return a;
-    }
-    else
-    {
-        return (-a);
-    }
+    return a + b;
 }
-<<<<<<< HEAD
-=======
+
+float resta(float a, float b)
+{
+    return a - b;
+}
+
+float multiplicacion(float a, float b)
+{
+    return a * b;
+}
+
+float division(float a, float b)
+{
+
+    return a / b;
+}
 
 float cuadrado(float a)
 {
@@ -122,4 +177,3 @@ float Menor(float a, float b)
         return b;
     }
 }
->>>>>>> CalculadoraV2
